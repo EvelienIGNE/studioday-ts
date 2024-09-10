18cf4e97c511d8af79013560e50888bc00daf5b5
Jorn Luiten <jorn@igne.nl>
2024-09-10 10:28:08 +0200

    simplified inline style object ex06

--- src/exercises/exercise06/index.tsx
+++ src/exercises/exercise06/index.tsx
@@ -6,23 +6,21 @@
 // For now lets use the below this method, because later we will play with this technique more.
 //
 // For this exercise, extend `TextProps` so you can pass along `variant` as a union of literal strings.
-// `variant` should be optional and default to "body"
+// `variant` should be optional and default to "base"
 
-const uglyInlineStyles = {
-  variants: {
-    base: {
-      font: "1rem/1.25",
-    },
-    intro: {
-      font: "italic small-caps 1.5rem/1.5",
-    },
+const textVariants = {
+  base: {
+    font: "1rem/1.25",
+  },
+  intro: {
+    font: "italic small-caps 1.5rem/1.5",
   },
 };
 
 type TextProps = React.PropsWithChildren<{}>;
 
 function Text({ variant, children }: TextProps) {
-  return <p style={uglyInlineStyles.variants[variant]}>{children}</p>;
+  return <p style={textVariants[variant]}>{children}</p>;
 }
 
 export default function Render() {
--- src/solutions/exercise06/index.tsx
+++ src/solutions/exercise06/index.tsx
@@ -6,16 +6,14 @@
 // For now lets use the below this method, because later we will play with this technique more.
 //
 // For this exercise, extend `TextProps` so you can pass along `variant` as a union of literal strings.
-// `variant` should be optional and default to "body"
+// `variant` should be optional and default to "base"
 
-const uglyInlineStyles = {
-  variants: {
-    base: {
-      font: "1rem/1.25",
-    },
-    intro: {
-      font: "italic small-caps 1.5rem/1.5",
-    },
+const textVariants = {
+  base: {
+    font: "1rem/1.25",
+  },
+  intro: {
+    font: "italic small-caps 1.5rem/1.5",
   },
 };
 
@@ -24,7 +22,7 @@ type TextProps = React.PropsWithChildren<{
 }>;
 
 function Text({ variant = "base", children }: TextProps) {
-  return <p style={uglyInlineStyles.variants[variant]}>{children}</p>;
+  return <p style={textVariants[variant]}>{children}</p>;
 }
 
 export default function Render() {
